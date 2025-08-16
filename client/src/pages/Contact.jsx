1fd957281e58f7c3827cbe45a2a6e2e3be75dd83
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-08-16 23:26:03 +0530

    Had to remove Contact and About us page cuz of some errors

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div>Had to remove Contact and About us page cuz of some errors</div>
     )
 }
 
